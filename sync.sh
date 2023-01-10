4a3b43bc884eeb9a1e79e5872d62109c8ceccdaf
ktownag <42381296+ktownag@users.noreply.github.com>
2023-01-10 20:23:01 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Execute first time after the machine is boot
-rclone sync /usr/local/app/script dropbox:elecv2p
+rclone sync /usr/local/app/script dropbox:v2p
 inotifywait -m -q -r -e create -e modify -e move -e delete --format '%w%f:%e' /usr/local/app/script | \
 while read; do \
     path=$(echo $REPLY | cut -d ":" -f 1); \
     event=$(echo $REPLY | cut -d ":" -f 2); \
     echo "$path was changed because of event $event"; \
     echo "Skipping $(timeout 3 cat | wc -l) further changes"; \
-    rclone sync --delete-during /usr/local/app/script dropbox:elecv2p; \
+    rclone sync --delete-during /usr/local/app/script dropbox:v2p; \
 done
